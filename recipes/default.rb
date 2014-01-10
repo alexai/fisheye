ad971f1f18f9edbb39675ea8c884273e1b07aa64
alexai <yai@dfengg.com>
2014-01-10 18:34:47 +0800

    up

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,18 +18,13 @@ remote_file "/usr/local/df/crucible-#{node['fisheye']['version']}.zip" do
 	source "#{node['fisheye']['url']}/crucible-#{node['fisheye']['version']}.zip"
 	owner "root"
 	mode "0644"
-	notifies :run, "execute[install fisheye]", :immediately
+	action :create
 end
 
-script "install fisheye" do
-	interpreter "bash"
-	user "root"
+execute "install fisheye" do
 	cwd "/usr/local/df"
-	action :run
-	code <<-EOH
-		unzip crucible-#{node['fisheye']['version']}.zip
-		ln -s ./fecru-#{node['fisheye']['version']} fecru
-	EOH
+	command "unzip crucible-#{node['fisheye']['version']}.zip;ln -s ./fecru-#{node['fisheye']['version']} fecru"
+	action :nothing
 end
 
 
